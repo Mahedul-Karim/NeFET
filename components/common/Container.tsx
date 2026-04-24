d9b1c3f1012ff6292b679c601dad6d6665afcd0b
Mahedul-Karim <karimrupo21@gmail.com>
2026-04-24 07:44:53 +0600

    initial project setup completed

---
+++ components/common/Container.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Container:React.FC<React.HTMLAttributes<HTMLDivElement>> = ({className,children}) => {
+  return (
+    <div className={`max-w-7xl w-11/12 mx-auto`}>{children}</div>
+  )
+}
+
+export default Container
